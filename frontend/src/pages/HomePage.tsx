e03b5bfa9e78d66daa6f8c4f8480adf77f80a8d7
root <root@dede84fbc996>
2025-07-23 05:14:36 +0000

    🎨 Add frontend code and user interface

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold underline">Financial Services Platform</h1>
+      <p>This is a placeholder for the home page.</p>
+    </div>
+  );
+};
+
+export default HomePage;
